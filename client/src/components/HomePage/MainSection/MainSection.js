38c95e0b880f20d9623c081892a31a5b0bf6566c
hoanganhquang <anhquang976@gmail.com>
2022-03-18 20:30:23 +0700

    update

--- client/src/components/HomePage/MainSection/MainSection.js
+++ client/src/components/HomePage/MainSection/MainSection.js
@@ -4,14 +4,37 @@ import Product from "./Product/Product";
 
 function MainSection() {
   return (
-    <section className="main">
-      <div className="container">
-        <div className="main-main">
-          <Category />
-          <Product />
+    <>
+      <section className="main">
+        <div className="container">
+          <div className="main-main">
+            <Category />
+            <Product />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="intro">
+        <div className="container">
+          <div className="intro-main">
+            <div className="content">
+              <h1 className="title">We build apps and websites</h1>
+              <p className="desc">
+                From multipurpose themes to niche templates, you’ll always find
+                something that catches your eye.
+              </p>
+              <div className="button-box">
+                <button className="primaryBtn">Liên hệ</button>
+                <p>or see our portfolio</p>
+              </div>
+            </div>
+
+            <div className="img-box">
+              <img src={require("../../../assets/images/intro.jpg")} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
