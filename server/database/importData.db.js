92d6b63e6f5cd0056755b13fa3c8df615ca55c1f
hoanganhquang <anhquang976@gmail.com>
2022-04-22 10:35:36 +0700

    update

--- server/database/importData.db.js
+++ server/database/importData.db.js
@@ -2,19 +2,19 @@ require("dotenv").config();
 const connectDb = require("./config.db");
 const fs = require("fs");
 
-const Product = require("../models/Product.model");
-// const Province = require("../models/Province.model");
+// const Product = require("../models/Product.model");
+const Province = require("../models/Province.model");
 // const Region = require("../models/Region.model");
 
 connectDb();
 
 const data = JSON.parse(
-  fs.readFileSync(`${__dirname}/product.data.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/province.data.json`, "utf-8")
 );
 
 const importData = async () => {
   try {
-    await Product.create(data);
+    await Province.create(data);
     console.log("Success");
   } catch (error) {
     console.log(error);
