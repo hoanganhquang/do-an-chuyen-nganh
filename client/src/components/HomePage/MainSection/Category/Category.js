17e22c864716931c9d1e46c2b98ab78ba789d2cb
hoanganhquang <anhquang976@gmail.com>
2022-04-23 18:20:03 +0700

    update

--- client/src/components/HomePage/MainSection/Category/Category.js
+++ client/src/components/HomePage/MainSection/Category/Category.js
@@ -1,20 +1,61 @@
 import Subscribe from "../Subscribe/Subscribe";
 import "./Category.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
 
 function Category() {
   return (
     <div className="category" data-aos="fade-right">
-      <div className="category-header">
-        <p className="title">Loại sản phẩm</p>
-      </div>
-      <div className="category-body">
-        <ul className="category-list">
-          <li className="category-item active">Trái cây</li>
-          <li className="category-item">Đồ uống</li>
-          <li className="category-item">Đồ uống</li>
-          <li className="category-item">Đồ uống</li>
-          <li className="category-item">Đồ uống</li>
-        </ul>
+      <div className="category-box">
+        <div className="category-header">
+          <p className="title">Địa phương</p>
+        </div>
+        <div className="category-body">
+          <div className="category-list">
+            <div className="category-item">
+              <div className="text-box">
+                Miền Bắc
+                <FontAwesomeIcon icon={faCaretRight} />
+              </div>
+              <ul className="province-list">
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+              </ul>
+            </div>
+            <div className="category-item ">
+              <div className="text-box">
+                Miền Bắc
+                <FontAwesomeIcon icon={faCaretRight} />
+              </div>
+              <ul className="province-list">
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+                <li>
+                  <a>Hà Nội</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
 
       <Subscribe />
